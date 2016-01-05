1b7cbef4ed1ac3af5e499e37977d9d151e0e8792
benorg <benorg@me.com>
2016-01-05 18:43:43 -0500

    Add to cart post request built in HTTPConnector, each thread needs its own cookie storer

--- src/backend/Item.java
+++ src/backend/Item.java
@@ -9,7 +9,10 @@ public class Item {
 	private String earlyLink;
 	private String size;
 	private int itemNumber;
-
+	
+	private String atcLink;
+	private String authenticityToken;
+	private String atcParameters;
 	private String link; //set by link finder once link is found
 	
 	public String[] getKeywords() {
@@ -72,6 +75,31 @@ public class Item {
 	public String toString() {
 		return "Item Number " + itemNumber + ":\n\nKeywords:\n\t" + Arrays.asList(keywords).toString() + "\nCategorylor:\n\t" + category + "\nColor:\n\t" + Arrays.asList(color).toString() + "\nSize:\n\t" + size + "\nEarly Link:\n\t" + earlyLink;
 	}
+
+	public String getAtcLink() {
+		return atcLink;
+	}
+
+	public void setAtcLink(String atcLink) {
+		this.atcLink = atcLink;
+	}
+
+	public String getAtcParameters() {
+		return atcParameters;
+	}
+
+	public void setAtcParameters(String atcParameters) {
+		this.atcParameters = atcParameters;
+	}
+
+	public String getAuthenticityToken() {
+		return authenticityToken;
+	}
+
+	public void setAuthenticityToken(String authenticityToken) {
+		this.authenticityToken = authenticityToken;
+	}
+	
 	
 	
 }
--- src/executor/HTTPConnector.java
+++ src/executor/HTTPConnector.java
@@ -1,13 +1,18 @@
 package executor;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
+import backend.Item;
 import backend.OrderSettings;
+import backend.RandomUserAgent;
 
 public class HTTPConnector {
 
@@ -59,10 +64,77 @@ public class HTTPConnector {
 		} catch (IOException e) {
 			success = false;
 		}
-		
+
 		return new Object[]{success, mostRecentHTML};
 	}
-	
+
+	public String atcPost(Item item) {
+		
+
+		
+		try {
+			String urlParameters = item.getAtcParameters();
+			String itemLink = item.getLink();
+			String xCSRFToken = item.getAuthenticityToken();
+			byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+			int postDataLength = postData.length;
+			URL url = new URL(item.getAtcLink());
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();           
+			conn.setDoOutput(true);
+			conn.setInstanceFollowRedirects(false);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Host", "www.supremenewyork.com");
+			conn.setRequestProperty("Connection", "keep-alive");
+			conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+			conn.setRequestProperty("Accept", "*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript");
+			conn.setRequestProperty("Origin", "http://www.supremenewyork.com");
+			conn.setRequestProperty("X-CSRF-Token", xCSRFToken);
+			conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+			conn.setRequestProperty("User-Agent", RandomUserAgent.getRandomUserAgent());
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			conn.setRequestProperty("Referer", itemLink);
+			conn.setRequestProperty("Accept-Encoding", "gzip, deflate");
+			conn.setRequestProperty("Accept-Language", "en-US,en;q=0.8");
+			
+//			POST /shop/169443/add HTTP/1.1
+//			Host: www.supremenewyork.com
+//			Connection: keep-alive
+//			Content-Length: 114
+//			Accept: */*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript
+//			Origin: http://www.supremenewyork.com
+//			X-CSRF-Token: j/3WH4FuSfPEDw9GkzSmeYrH14VZrcI1S1KXJO+24/8=
+//			X-Requested-With: XMLHttpRequest
+//			User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36
+//			Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+//			Referer: http://www.supremenewyork.com/shop/pants/eat-me-sweatshort
+//			Accept-Encoding: gzip, deflate
+//			Accept-Language: en-US,en;q=0.8
+//
+//			utf8=%E2%9C%93&authenticity_token=RWvrnsyge9GlIfS5rX63S5p%2B5J%2Bcd6hecMFnhZ9XjQk%3D&size=28959&commit=add+to+cart
+			
+
+			conn.setUseCaches(false);
+			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+			wr.write(postData);
+		
+			conn.connect();
+			
+			
+			return connectionToString(conn);
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		return null;
+
+	}
+
 	private String connectionToString(URLConnection con) throws IOException {
 		String mostRecentHTML = "";
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
